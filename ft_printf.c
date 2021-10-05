2efbf9b1cd3901f13651aa96a3f98871dd489c07
David Gutin <david14av@gmail.com>
2021-10-05 14:11:11 +0200

    figuring things out

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,12 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 13:28:59 by dgutin            #+#    #+#             */
-/*   Updated: 2021/10/04 15:35:24 by dgutin           ###   ########.fr       */
+/*   Updated: 2021/10/05 14:09:02 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
+/*
 void	ft_set_points(t_conv *conv)
 {
 	conv->c = 0;
@@ -23,19 +23,44 @@ void	ft_set_points(t_conv *conv)
 	conv->x1 = 0;
 	conv->x2 = 0;
 }
+*/
+int		ft_is_flag(char c)
+{
+	return (c == 99 || c == 115 || c == 112 || c == 100 || c == 105 || c == 117 ||
+			c == 120 || c == 88 || c == 37)
+}
+
+void	ft_check_flags(char *str, int i)
+{
+
+}
 
 int	ft_printf(const char *str, ...)
 {
-	int		ret;
-	t_conv	*conv;
+	int				ret;
+	int				i;
+	int				x;
+	char			flags[10];
+//	t_conv			*conv;
+	static t_tab	tab[7] = {c_flag, s_flag, p_flag, d_flag, u_flag, x_flag, 
+			prct_flag};
 
 	if (!str)
 		return (0);
 	ret = 0;
+	i = 0;
+	x = 0;
+	flags = "cspdiuxX%";
 	va_start(g_conv, str);
-	while (str)
+	while (str[i])
 	{
+		while (str[i] != flags[x] && flags[x])
+			x++;
+		if (x == 7)
+			write(1, str[i], 1);
+//		ft_set_points(conv);
+		i++;
 	}
-	ft_set_points(conv);
+	va_end(g_conv;)
 	return (ret);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 12:36:03 by dgutin            #+#    #+#             */
-/*   Updated: 2021/10/04 15:09:20 by dgutin           ###   ########.fr       */
+/*   Updated: 2021/10/05 13:18:43 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 # define FT_PRINTF_H
 
 # include "libft/libft.h"
+# include <unistd.h>
 # include <stdio.h>
 # include <stdarg.h>
 
 int		ft_printf(const char *str, ...);
 
+va_list			g_conv;
+
+typedef int		(*t_tab)(int a, int b);
+
 typedef struct s_conv
 {
-	va_list		conv;
 	int			c;
 	int			s;
 	int			p;
 	int			d;
 	int			u;
-	int			i;
-	int			x1;
-	int			x2;
+	int			x;
 }				t_conv;
 
 #endif
